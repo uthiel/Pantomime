a76066b9cac20e3fe6b1fab08676dce053e937dc
Peter Woods <pwoods@artandlogic.com>
2020-09-02 15:14:08 -0700

    Update Package.swift to provide Xcode 11 compatibility

--- Package.swift
+++ Package.swift
@@ -1,5 +1,17 @@
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
-    name: "Pantomime"
+    name: "Pantomime",
+    platforms: [
+        .iOS(.v10), .tvOS(.v10)
+    ],
+    products: [
+        .library(name: "Pantomime", targets: ["Pantomime"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "Pantomime", dependencies: [], path: "sources")
+    ],
+    swiftLanguageVersions: [.v5]
 )
